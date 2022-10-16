3dea6ef2649401f69cce8d68aa202c2cb2494b55
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-10-16 16:29:27 +0100

    Add example code for coverage reports

--- crates/project-template/src/lib.rs
+++ crates/project-template/src/lib.rs
@@ -1,1 +1,15 @@
 //! A project template for the quick instantiation of rust projects.
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(add_two(2), 4);
+    }
+}
